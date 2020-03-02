9d12a33294b3fa0c1875f4ae0b410ffe29555a40
23432 <werewr@mail.ru>
2020-03-02 21:07:02 +0300

    Initial Commit

---
+++ Remarks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remarks
+//
+//  Created by val on 3/2/20.
+//  Copyright © 2020 VL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
